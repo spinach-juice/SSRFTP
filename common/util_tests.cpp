b174aa7d42064e7760b6a8a533ac006d8315b553
Justin Nielson <just806n@gmail.com>
2019-10-31 14:54:51 -0500

    Added tests for all utility functions

--- common/util_tests.cpp
+++ common/util_tests.cpp
@@ -1,12 +1,5 @@
 #include "catch.hpp"
 #include "util.h"
-/*
-void ulong_array_sort(unsigned long * array, unsigned long const length);
-unsigned long ulong_array_max(unsigned long const * const array, unsigned long const length);
-unsigned long ulong_array_min(unsigned long const * const array, unsigned long const length);
-std::vector<unsigned long> ulong_array_singles(unsigned long const * const array, unsigned long const length);
-std::vector<unsigned long> ulong_array_ranges(unsigned long const * const array, unsigned long const length);
-*/
 
 TEST_CASE("Utility Functions: uchar_array_equal")
 {
@@ -78,4 +71,71 @@ TEST_CASE("Utility Functions: hex2ascii")
 	char valid_output[] = "af4389761cc798aa";
 
 	CHECK(schar_array_equal(ascii, valid_output, 16));
+}
+
+TEST_CASE("Utility Functions: ulong_array_max")
+{
+	unsigned long array[] = {0x6789, 0xaaaa, 0x1234, 0xdddd, 0x4321, 0xbbbb, 0x9876, 0xcccc};
+
+	unsigned long max = ulong_array_max(array, 8);
+
+	unsigned long valid_output = 0xdddd;
+
+	CHECK(max == valid_output);
+}
+
+TEST_CASE("Utility Functions: ulong_array_min")
+{
+	unsigned long array[] = {0x6789, 0xaaaa, 0x1234, 0xdddd, 0x4321, 0xbbbb, 0x9876, 0xcccc};
+
+	unsigned long min = ulong_array_min(array, 8);
+
+	unsigned long valid_output = 0x1234;
+
+	CHECK(min == valid_output);
+}
+
+TEST_CASE("Utility Functions: ulong_array_sort")
+{
+	unsigned long array[] = {0x6789, 0xaaaa, 0x1234, 0xdddd, 0x4321, 0xbbbb, 0x9876, 0xcccc};
+
+	ulong_array_sort(array, 8);
+
+	unsigned long valid_output[] = {0x1234, 0x4321, 0x6789, 0x9876, 0xaaaa, 0xbbbb, 0xcccc, 0xdddd};
+
+	CHECK(ulong_array_equal(array, valid_output, 8));
+}
+
+TEST_CASE("Utility Functions: ulong_array_singles")
+{
+	unsigned long missing_nums[100] = {0x0000, 0x0003, 0x4158, 0x1122, 0x5673, 0xaaaa, 0xf437, 0x0005, 0x0010, 0x0020};
+
+	int i = 10;
+	for(; i < 100; i++)
+		missing_nums[i] = missing_nums[i - 1] + 1;
+
+	std::vector<unsigned long> valid_output = {0x0000, 0x0003, 0x0005, 0x0010, 0x1122, 0x4158, 0x5673, 0xaaaa, 0xf437};
+
+	ulong_array_sort(missing_nums, 100);
+
+	std::vector<unsigned long> output = ulong_array_singles(missing_nums, 100);
+
+	CHECK(output == valid_output);
+}
+
+TEST_CASE("Utility Functions: ulong_array_ranges")
+{
+	unsigned long missing_nums[100] = {0x0000, 0x0003, 0x4158, 0x1122, 0x5673, 0xaaaa, 0xf437, 0x0005, 0x0010, 0x0020};
+
+	int i = 10;
+	for(; i < 100; i++)
+		missing_nums[i] = missing_nums[i - 1] + 1;
+
+	std::vector<unsigned long> valid_output = {0x0020, 0x007a};
+
+	ulong_array_sort(missing_nums, 100);
+
+	std::vector<unsigned long> output = ulong_array_ranges(missing_nums, 100);
+
+	CHECK(output == valid_output);
 }
