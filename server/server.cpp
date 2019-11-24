5eaccb7be84d531dbe110576f91818a1f765f4fa
Sherv31 <sherwynbraganza@gmail.com>
2019-11-23 23:22:57 -0700

    Server recognizes shard type and outputs it to files

--- server/server.cpp
+++ server/server.cpp
@@ -22,6 +22,9 @@ void server::start_server()
         
         if(server_comm->message_available())
         {   
+        
+            /*-------current data storage variables for packets-----*/
+            // common
             unsigned short trans_id;
             
             // client start packet params
@@ -40,12 +43,25 @@ void server::start_server()
             
             switch(curr.int_type())
             {
+                // client start packet
                 case(0):
                     if(interpret_client_start(curr, md5_chksum, 
                         file_size, num_shards, trans_id, 
                             destination_path, path_length))
+                    {
+                        filename = "shard/clientstart.shrd";
+                        
+                        // do I print out different variants of file sizes?
+                        file << md5_chksum << endl
+                            << file_size << endl
+                            << num_shards << endl
+                            << trans_id << endl
+                            << path_length << endl
+                            << destination_path << endl;
+                    }
                     break;
                 
+                // normal shard packet
                 case(1):
                     if(interpret_file_shard(curr, shard_num,
                         trans_id, shard_data, data_size))
@@ -53,6 +69,11 @@ void server::start_server()
                         filename = "shard/" + to_string(shard_num)
                             +".shrd";
                         file.open(filename, ios::out | ios::trunc);
+                        file << shard_num << endl 
+                            << trans_id << endl
+                            << data_size << endl
+                            << shard_data << endl; 
+                            
                         file.close();
                     }
                     break;
@@ -68,6 +89,13 @@ void server::start_server()
 void server::kill()
 {
     server_comm->kill();
+}
+
+int main()
+{
+    server test_server;
+    
+    //server.start_server();
 }
 
 
