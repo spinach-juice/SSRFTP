d6ccac6b22e25e818ef51a969b33d82a0ab27f59
Justin Nielson <just806n@gmail.com>
2020-04-20 03:38:49 +0000

    First implementation of semi robust client

--- client/client.cpp
+++ client/client.cpp
@@ -12,14 +12,27 @@
 #include <cstring>
 #include <cstdio>
 #include "shard_manager.h"
+#include <cstdlib>
+#include <time.h>
 
 int main(int argc, char** argv)
 {
-	// Command line arguments: ./SSRFTP_client filename endpoint
-	if(argc != 3)
+	// Command line arguments: ./SSRFTP_client filename endpoint [semi-robust_#_times]
+	if(argc != 3 && argc != 4)
 	{
-		std::cout << "Usage: SSRFTP_client filename endpoint" << std::endl;
+		std::cout << "Usage: SSRFTP_client filename endpoint [semi-robust_#_times]" << std::endl;
 		return 1;
+	}
+
+	srand(time(NULL));
+
+	bool semi_robust_mode = false;
+	unsigned int number_of_transfers = 0;
+
+	if(argc == 4)
+	{
+		number_of_transfers = atoi(argv[3]);
+		std::cout << "Using semi-robust mode with " << number_of_transfers << " repeats\n";
 	}
 
 	char file_to_transfer[261] = {0};
@@ -39,16 +52,50 @@ int main(int argc, char** argv)
 	long file_size = ftell(open_file);
 	fclose(open_file);
 
-	unsigned long tot_shards = (unsigned long)file_size / 65495;
-	if((unsigned long)file_size % 65495)
+	unsigned long tot_shards = (unsigned long)file_size / SHARD_SIZE_MAX;
+	if((unsigned long)file_size % SHARD_SIZE_MAX)
 		tot_shards++;
 
-	unsigned short trans_id = 1337;
+	unsigned short trans_id = rand() % 65536;
 	unsigned short path_length = (unsigned short)strlen(file_to_transfer);
+
+	// If we are in semi-robust mode, handle that here:
+	if(semi_robust_mode)
+	{
+		Communicator sr_com;
+		ShardManager sr_sm(file_to_transfer, trans_id);
+
+		for(unsigned long j = 0; j < number_of_transfers; j++)
+		{
+			for(unsigned long i = 0; i < tot_shards; i++)
+			{
+				if(sr_sm.shard_available(i))
+				{
+					unsigned char* shard_data;
+					unsigned short shard_size;
+
+					shard_data = sr_sm.get_shard_data(i, shard_size);
+
+					if(i == tot_shards - 1)
+					{
+						Packet p = build_semi_robust_shard(trans_id, true, i, shard_data, shard_size);
+						sr_com.send_message(package_message(p, (std::string)endpoint));
+					}
+					else
+					{
+						Packet p = build_semi_robust_shard(trans_id, false, i, shard_data, shard_size);
+						sr_com.send_message(package_message(p, (std::string)endpoint));
+					}
+				}
+			}
+		}
+
+		return 0;
+	}
+	// END OF SEMI ROBUST MODE
 
 	char md5_chk[33] = {0};
 	MD5(file_to_transfer, md5_chk);
-	std::cout << "client start packet:\nmd5 = " << md5_chk << "\nfile size = " << file_size << "\nnumber of shards = " << tot_shards << "\ntrans id = " << trans_id << "\nfile being transferred = " << file_to_transfer << std::endl;
 	Packet client_start = build_client_start(md5_chk, file_size, tot_shards, trans_id, file_to_transfer, path_length);
 
 	Communicator com;
